fde45d93013a656a6c297992a56fb2e023414d73
Andrés Di Giovanni <agiovanni@jbctools.com>
2019-05-16 07:41:47 +0200

    Added ObjectExtensions

---
+++ src/ADN.Extensions/Models/ObjectExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace ADN.Extensions
+{
+    /// <summary>
+    /// A static class of extension methods for <see cref="Object"/>.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Clone a serializable object.
+        /// </summary>
+        /// <param name="source">Object to clone.</param>
+        /// <returns>A clone of the object.</returns>
+        /// <example>
+        /// <code lang="csharp">
+        /// var person1 = new Person()
+        /// {
+        ///     FirstName = "Luke",
+        ///     LastName = "Skywalwer"
+        /// };
+        /// var person2 = person1.DeepClone();
+        /// </code>
+        /// </example>
+        public static T DeepClone<T>(this T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable", "source");
+            }
+
+            if (source is null)
+            {
+                return default;
+            }
+
+            var formatter = new BinaryFormatter();
+            using (Stream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ tests/ADN.Extensions.Tests/Models/ObjectExtensionsTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ADN.Extensions.Tests
+{
+    public class ObjectExtensionsTest
+    {
+        [Fact]
+        public void DeepClone()
+        {
+            var person1 = new Person()
+            {
+                FirstName = "Luke",
+                LastName = "Skywalwer"
+            };
+            var person2 = person1.DeepClone();
+
+            Assert.True(PersonsAreEqual(person1, person2));
+        }
+
+        [Fact]
+        public void DeepClone_ChangeObject()
+        {
+            var person1 = new Person()
+            {
+                FirstName = "Luke",
+                LastName = "Skywalwer"
+            };
+            var person2 = person1.DeepClone();
+            person2.FirstName = "Mike";
+
+            Assert.False(PersonsAreEqual(person1, person2));
+        }
+
+        [Fact]
+        public void DeepClone_Null()
+        {
+            Person person1 = null;
+            var person2 = person1.DeepClone();
+
+            Assert.Null(person2);
+        }
+
+        [Fact]
+        public void DeepClone_ArgumentException_NonSerializable()
+        {
+            var person1 = new Person_NonSerializable()
+            {
+                FirstName = "Luke",
+                LastName = "Skywalwer"
+            };
+
+            Assert.Throws<ArgumentException>(() => person1.DeepClone());
+        }
+
+        public bool PersonsAreEqual(Person person1, Person person2)
+        {
+            return person1.FirstName == person2.FirstName &&
+                person1.LastName == person2.LastName;
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Person_NonSerializable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
