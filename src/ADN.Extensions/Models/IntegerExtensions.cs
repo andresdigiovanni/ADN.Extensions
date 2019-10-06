12ff60fd962c8c74f18e3d8a4ca3970560e58afc
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2019-10-06 14:10:40 +0200

    Added IntegerExtensions

---
+++ src/ADN.Extensions/Models/IntegerExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADN.Extensions
+{
+    /// <summary>
+    /// A static class of extension methods for <see cref="Int32"/>.
+    /// </summary>
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Get the next power 2 value.
+        /// </summary>
+        /// <param name="x">Value to get the next power 2.</param>
+        /// <returns>Power 2 value.</returns>
+        /// <example>
+        /// <code lang="csharp">
+        /// int value = 29;
+        /// var nextPow2 = value.NextPow2();
+        /// 
+        /// /*
+        /// result is 32
+        /// */
+        /// </code>
+        /// </example>
+        public static int NextPow2(this int x)
+        {
+            x--;
+            x |= (x >> 1);
+            x |= (x >> 2);
+            x |= (x >> 4);
+            x |= (x >> 8);
+            x |= (x >> 16);
+            return (x + 1);
+        }
+    }
+}
---
+++ tests/ADN.Extensions.Tests/Models/IntegerExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ADN.Extensions.Tests
+{
+    public class IntegerExtensionsTest
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 4)]
+        [InlineData(4, 4)]
+        [InlineData(5, 8)]
+        [InlineData(6, 8)]
+        [InlineData(7, 8)]
+        [InlineData(8, 8)]
+        [InlineData(9, 16)]
+        [InlineData(15, 16)]
+        [InlineData(16, 16)]
+        [InlineData(17, 32)]
+        [InlineData(31, 32)]
+        [InlineData(32, 32)]
+        public void NextPow2(int value, int expected)
+        {
+            var result = value.NextPow2();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
